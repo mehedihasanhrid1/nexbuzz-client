ee122aecf40d5dfc8baf21cca5ad2ad872e3d617
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2023-12-24 20:40:04 +0600

    Home page added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
         <div>
-            Hello
+            <Outlet/>
         </div>
     );
 };
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,11 +1,20 @@
 import {createBrowserRouter} from "react-router-dom";
 import Root from "../layouts/Root";
+import ErrorPage from "../components/Errorpage";
+import Home from "../pages/home/Home";
 
 const routers = createBrowserRouter(
     [
         {
             path:'/',
-            element: <Root/>
+            element: <Root/>,
+            errorElement:<ErrorPage/>,
+            children:[
+                {
+                    path:"/",
+                    element:<Home/>
+                }
+            ]
         }
     ]
 );
