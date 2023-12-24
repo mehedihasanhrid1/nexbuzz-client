73ed43d1c101684850f967c03d493ca98c091fc7
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2023-12-24 20:44:49 +0600

    Dark Mode added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Root = () => {
+    const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('darkMode') === 'true');
+
+  useEffect(()=>{
+    window.scrollTo(0, 0);
+  },[location.pathname]);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', isDarkMode);
+  }, [isDarkMode]);
+  
     return (
         <div>
             <Outlet/>
